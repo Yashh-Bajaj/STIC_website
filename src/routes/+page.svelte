6733fc0ff829e9599856e5a0be4011a180feb30a
Aradhya Tiwari <aradhya.prad@gmail.com>
2023-03-30 23:41:10 +0530

    vite hot reload code added, mentor section added (incomplete)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Mentor from "./Mentor.svelte";
+</script>
+
+<main class="bg-[#454545] text-white">
+    <Mentor />
+</main>
---
+++ src/routes/Mentor.svelte
@@ -0,0 +1,17 @@
+<main>
+    <h1>Chief Advisor</h1>
+    <!-- nikita mam wala section-->
+    <div class="flex justify-between">
+        <div class="bg[url()] border-[1px] border-slate-100" />
+        <div class="">
+            <h2>Prof.Nitika Vats Doohan</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo id,
+                natus quisquam beatae sequi ab aut hic, sed maiores voluptatibus
+                optio fuga officia illum autem aliquid nobis labore ipsum,
+                deserunt nulla illo.
+            </p>
+        </div>
+    </div>
+    <!--Dusre Sir wala section -->
+</main>
